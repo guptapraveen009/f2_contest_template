3356067fd1e1ba11ff57743f735fcb7582153126
GuptaPraveenKumar <149958810+guptapraveen009@users.noreply.github.com>
2024-08-18 09:26:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,49 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
-}
+  arr.map (empolyee => {//map iterate on each element of object and if profession become developer trueit executes
+    if (empolyee.profession === "developer"){
+       console.log(empolyee.name);
+    }
+  });
+  }
+PrintDeveloperbyMap();
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(empolyee=>{//it is same as map
+    if(empolyee.profession === "developer"){
+      console.log(empolyee.name);
+    }
+  });
 }
+PrintDeveloperbyForEach();
 
 function addData() {
   //Write your code here, just console.log
+  arr.id = "4", arr.name = "praveen", arr.age = "19", arr.profession = "developer";//it is the way add new object in the array
+    console.log(arr);
+  
 }
+addData();
 
 function removeAdmin() {
   //Write your code here, just console.log
+  let newarr = arr.filter(empolyee => empolyee.profession !=="admin");//filter it iterate on each element of object and gives the element whose profession is not admin
+    console.log(newarr);
 }
+ removeAdmin();
+
+ let Newarr = [
+    {id:4,name:"rahul",age:"20",profession:"java developer"},
+    {id:5,name:"prakash",age:"25",profession:"frontend developer"},
+    {id:6,name:"ram",age:"20",profession:"backend developer"},
+    {id:7,name:"rakesh",age:"20",profession:"fullstack developer"}
+  ]
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let temp = arr.concat(Newarr);
+  console.log(temp);
 }
+  concatenateArray();
